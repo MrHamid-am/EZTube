e9833f6d120ba0e89cd51514eb9295e1fa99f99e
hamid <alimohamadihamidreza560@gmail.com>
2020-10-23 08:45:39 -0700

    Designed ListView for QueryListView.Xaml File

--- EZTube/ViewModels/Query_And_Processing/QueryListViewModel.cs
+++ EZTube/ViewModels/Query_And_Processing/QueryListViewModel.cs
@@ -23,12 +23,12 @@ namespace EZTube.ViewModels.Query_And_Processing
         {
             DownloadsList = new BindableCollection<YouTubeVideo>()
             {
-                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"),
-                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"),
-                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"),
-                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"),
-                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"),
-                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"),
+                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"){ProgressState = 50},
+                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"){ProgressState = 25},
+                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"){ProgressState = 30},
+                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"){ProgressState = 60},
+                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"){ProgressState = 80},
+                new YouTubeVideo("3 Scary Game","Mark","3 scary games number 1 bla bla bla...","https://d12avs59aaiyvm.cloudfront.net/wp-content/uploads/2018/02/Markiplier-youtube.jpg"){ProgressState = 100},
             };
         }
     }
