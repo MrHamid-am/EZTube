183d980d95d9360ad340687b906a10b2a1a5f48c
hamid <alimohamadihamidreza560@gmail.com>
2020-10-26 08:31:38 -0700

    Added IViewFactory And it's Class files

---
+++ EZTube/Framework/ViewFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EZTube.Views.Pages;
+
+namespace EZTube.Framework
+{
+    public class ViewFactory:IViewFactory
+    {
+        public SettingsView CeateSettingsView()
+        {
+            return new SettingsView();
+        }
+    }
+}
