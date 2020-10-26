438cac8ed36d080e4e09a68500d2368653f95123
hamid <alimohamadihamidreza560@gmail.com>
2020-10-26 08:31:07 -0700

    Added IViewModelFactoryBinder and it's class files

---
+++ EZTube/Framework/ViewModelBinderFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EZTube.ViewModels.Pages;
+using EZTube.Views.Pages;
+
+namespace EZTube.Framework
+{
+    //This Class Will Create View And ViewModel Both And Bind ViewModel To it's View
+    public class ViewModelBinderFactory : IViewModelBinderFactory
+    {
+        private readonly IViewModelFactory _viewModelFactory;
+        private readonly IViewFactory _viewFactory;
+
+        public ViewModelBinderFactory(IViewModelFactory viewModelFactory, IViewFactory viewFactory)
+        {
+            //ViewModel Factory To Create ViewModel
+            _viewModelFactory = viewModelFactory;
+            
+            //View Factory To Create View
+            _viewFactory = viewFactory;
+        }
+
+        public SettingsView CreateAndBindSettingsViewModel(object creationContext = null)
+        {
+            //Create View And ViewModel With Factories
+            var viewModel = _viewModelFactory.CreateSettingsViewModel();
+            var view = _viewFactory.CeateSettingsView();
+
+            //Bind ViewModel To It's View
+            Caliburn.Micro.ViewModelBinder.Bind(viewModel, view, creationContext);
+
+            return view;
+        }
+    }
+}
