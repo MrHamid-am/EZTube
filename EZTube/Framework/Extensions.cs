858fbb4d418971597e0b7099e21eb05819708753
hamid <alimohamadihamidreza560@gmail.com>
2020-10-31 17:11:41 +0330

    Added SingleDownload Page Added Extension Methods For SingleDownload Page

--- EZTube/Framework/Extensions.cs
+++ EZTube/Framework/Extensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Caliburn.Micro;
 using EZTube.ViewModels.Pages;
+using EZTube.Views.Pages;
 
 namespace EZTube.Framework
 {
@@ -10,6 +12,28 @@ namespace EZTube.Framework
         public static SettingsViewModel CreateSettingsViewModel(this IViewModelFactory factory)
         {
             return new SettingsViewModel();
+        }
+
+        public static SingleDownloadViewModel CreateSingleDownloadViewModel(this IViewModelFactory factory)
+        {
+            return new SingleDownloadViewModel();
+        }
+
+        public static SingleDownloadView CreateSingleDownloadView(this IViewFactory factory)
+        {
+            return new SingleDownloadView();
+        }
+
+        public static SingleDownloadView CreateAndBindSingleDownloadViewModel(this IViewModelBinderFactory binderFactory,object creationContext = null)
+        {
+            //Create View And ViewModel With Factories
+            var viewModel = IoC.Get<IViewModelFactory>().CreateSingleDownloadViewModel();
+            var view = IoC.Get<IViewFactory>().CreateSingleDownloadView();
+
+            //Bind ViewModel To It's View
+            Caliburn.Micro.ViewModelBinder.Bind(viewModel, view, creationContext);
+
+            return view;
         }
     }
 }
---
+++ EZTube/ViewModels/Pages/SingleDownloadViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Caliburn.Micro;
+
+namespace EZTube.ViewModels.Pages
+{
+    public class SingleDownloadViewModel:Screen
+    {
+
+    }
+}
