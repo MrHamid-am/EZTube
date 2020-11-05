23a604811073442c702dd3d0dad2e5bb7c6ffcd2
hamid <alimohamadihamidreza560@gmail.com>
2020-11-05 18:45:35 +0330

    Added Query Model

---
+++ EZTube/Models/Query.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EZTube.Models
+{
+    public class Query
+    {
+        public QueryKind QueryKind { get; set; }
+        public string Value { get; set; }
+
+        public Query(QueryKind queryKind, string value)
+        {
+            QueryKind = queryKind;
+            Value = value;
+        }
+    }
+
+    public enum QueryKind
+    {
+        Video,
+        Playlist,
+        Channel,
+        User,
+        Search
+    }
+}
