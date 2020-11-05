18b5d735472ac18d2cfcecfe6099ea2a5f0b9ec9
hamid <alimohamadihamidreza560@gmail.com>
2020-11-05 19:30:16 +0330

    Added Comments to Created Models

--- EZTube/Models/ExecutedQuery.cs
+++ EZTube/Models/ExecutedQuery.cs
@@ -5,10 +5,25 @@ using YoutubeExplode.Videos;
 
 namespace EZTube.Models
 {
+    /*
+     * This is ExecutedQuery Class
+     * As the name implies this Class is The Result of Query Execution
+     */
     public class ExecutedQuery
     {
+        //Processed Query
         public Query Query { get; set; }
+
+        /*
+         * a Readonly List of Videos And Their Details
+         * Which is Result of Query Execution (Depends on QueryKind it Will Be Different such as Playlist Videos, Channel Videos,etc.)
+         */
         public IReadOnlyList<Video> Videos { get; set; }
+
+        /*
+         * Title of Executed Query
+         * Depends on QueryKind it Will Be Different such as Playlist Title, Channel Name,etc.
+         */
         public string Title { get; set; }
 
         public ExecutedQuery(Query query, IReadOnlyList<Video> videos, string title)
--- EZTube/Models/Query.cs
+++ EZTube/Models/Query.cs
@@ -4,9 +4,16 @@ using System.Text;
 
 namespace EZTube.Models
 {
+    /*
+     * This is Query class
+     * Which Will be Processed And Executed
+     */
     public class Query
     {
+        //Kind of The Query. Represent Which Query Type is Executing
         public QueryKind QueryKind { get; set; }
+
+        //Value of The Query. its Depend on Query Kind. it Can Be ChannelId,UserId,etc.
         public string Value { get; set; }
 
         public Query(QueryKind queryKind, string value)
