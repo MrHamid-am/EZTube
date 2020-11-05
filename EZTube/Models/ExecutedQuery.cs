0355bc561539fda0a366b433b9b2f12de55d1e42
hamid <alimohamadihamidreza560@gmail.com>
2020-11-05 19:01:56 +0330

    Added ExecutedQuery Model

---
+++ EZTube/Models/ExecutedQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YoutubeExplode.Videos;
+
+namespace EZTube.Models
+{
+    public class ExecutedQuery
+    {
+        public Query Query { get; set; }
+        public IReadOnlyList<Video> Videos { get; set; }
+        public string Title { get; set; }
+
+        public ExecutedQuery(Query query, IReadOnlyList<Video> videos, string title)
+        {
+            Query = query;
+            Videos = videos;
+            Title = title;
+        }
+    }
+}
