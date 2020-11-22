162910e07b5e298c26771a125c3b4598fdc9d47c
hamid <alimohamadihamidreza560@gmail.com>
2020-11-22 19:37:58 +0330

    Fixed Injection Problem for ViewModelFactory (container was injecting null value)

--- EZTube/Framework/ViewModelFactory.cs
+++ EZTube/Framework/ViewModelFactory.cs
@@ -12,9 +12,6 @@ namespace EZTube.Framework
     {
         private readonly IEventAggregator _eventAggregator;
 
-        [Dependency]
-        public IViewModelBinderFactory ViewModelBinderFactory { get; set; }
-
         public ViewModelFactory(IEventAggregator _eventAggregator)
         {
             this._eventAggregator = _eventAggregator;
@@ -22,7 +19,8 @@ namespace EZTube.Framework
 
         public QueryBoxViewModel CreateQueryBoxViewModel(QueryService queryService)
         {
-            var instance = new QueryBoxViewModel(_eventAggregator,queryService, ViewModelBinderFactory);
+            var viewModelBinderFactory = IoC.Get<IViewModelBinderFactory>();
+            var instance = new QueryBoxViewModel(_eventAggregator,queryService, viewModelBinderFactory);
 
             return instance;
         }
