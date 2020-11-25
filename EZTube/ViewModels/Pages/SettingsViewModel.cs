b7486b7d0af04fa790ed953a82406d2746bb76f6
hamid <alimohamadihamidreza560@gmail.com>
2020-11-25 17:45:38 +0330

    Removed Temporary Codes from SettingsViewModel

--- EZTube/ViewModels/Pages/SettingsViewModel.cs
+++ EZTube/ViewModels/Pages/SettingsViewModel.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 using Caliburn.Micro;
+using EZSettings;
+using EZTube.Models;
 
 namespace EZTube.ViewModels.Pages
 {
     public class SettingsViewModel:Screen
     {
-        //Fody Property Changed Package Will Notify Changes Automatically
-        public int MaxDownloads { get; set; }
-        public string FileNameTemplate { get; set; }
+        private readonly SettingsManager<DownloadSettings> _settingsManager;
+
+        public SettingsViewModel(SettingsManager<DownloadSettings> settingsManager)
+        {
+            _settingsManager = settingsManager;
+        }
+
+        protected override void OnViewLoaded(object view)
+        {
+            //Load Settings When View is Ready
+            _settingsManager.LoadSettings();
+            base.OnViewLoaded(view);
+        }
     }
 }
