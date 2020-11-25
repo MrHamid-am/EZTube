4bb5dc21d02d7f278440a136ad0eb9e23d6843c0
hamid <alimohamadihamidreza560@gmail.com>
2020-11-25 17:31:22 +0330

    Addded DownloadSettings Model

---
+++ EZTube/Models/DownloaderSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Serialization;
+using YoutubeExplode.Videos.Streams;
+
+namespace EZTube.Models
+{
+    public class DownloadSettings : INotifyPropertyChanged
+    {
+        public int MaxConCurrentDownloads { get; set; } = 2;
+
+        public string FileNameTemplate { get; set; }
+
+        public IReadOnlyList<string> ExcludedContainerFormats { get; set; }
+
+        public bool IsAutoUpdate { get; set; } = true;
+
+        public bool IsDarkMode { get; set; }
+
+        public bool InjectMediaTags { get; set; } = true;
+
+        public bool SkipExistingFiles { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
