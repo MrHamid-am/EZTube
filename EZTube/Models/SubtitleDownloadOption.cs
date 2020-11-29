a120bef1a4866c7295c374c9d58e369de9a550d8
hamid <alimohamadihamidreza560@gmail.com>
2020-11-29 18:14:44 +0330

    Add SubtitleDownloadOption Model

---
+++ EZTube/Models/SubtitleDownloadOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode.Videos.ClosedCaptions;
+
+namespace EZTube.Models
+{
+    public class SubtitleDownloadOption
+    {
+        public ClosedCaptionTrackInfo TrackInfo { get; }
+
+        public SubtitleDownloadOption(ClosedCaptionTrackInfo trackInfo)
+        {
+            TrackInfo = trackInfo;
+        }
+
+        public override string ToString() => TrackInfo.Language.ToString();
+    }
+}
